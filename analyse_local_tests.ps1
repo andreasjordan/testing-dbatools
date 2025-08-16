360e4a076d512044235fd7c2c604d5b221642ed1
andreasjordan <anj@ordix.de>
2025-08-16 13:41:33 +0200

    Some updates

--- analyse_local_tests.ps1
+++ analyse_local_tests.ps1
@@ -8,14 +8,13 @@ $testingBase = "$githubBase\testing-dbatools"
 $configFile = "$testingBase\TestConfig_local_instanes.ps1"
 $logPath    = "$testingBase\logs"
 
-$resultsFileName = "$logPath\results_$([datetime]::Now.ToString('yyyMMdd_HHmmss')).txt"
-
 Import-Module -Name "$dbatoolsBase\dbatools.psm1" -Force
 $null = Set-DbatoolsInsecureConnection
 
 $TestConfig = Get-TestConfig -LocalConfigPath $configFile
 
-$null = Restart-DbaService -SqlInstance $TestConfig.instance1, $TestConfig.instance2, $TestConfig.instance2 -Type Engine -Force
+
+
 
 $results = Get-Content -Path "$logPath\results_*.txt" | ConvertFrom-Json
 $selected = $results | ogv -PassThru

5e6a360f61507a4bf4123b1ff3671e188a111fc5
Andreas Jordan <anj@ordix.de>
2026-08-25 16:52:31 +0200

    Do not check the new InModule.RestoreDatabaseContext test file for a matching command

--- TestTestfiles.Tests.ps1
+++ TestTestfiles.Tests.ps1
@@ -15,6 +15,7 @@ BeforeDiscovery {
         'dbatools.Tests.ps1'
         'InModule.Commands.Tests.ps1'
         'InModule.Help.Tests.ps1'
+        'InModule.RestoreDatabaseContext.Tests.ps1'
         'InModule.TypeExtensions.Tests.ps1'
     )
     $testFile = Get-ChildItem -Path "$TestPath\*.Tests.ps1" |
